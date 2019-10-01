9ffcf07850d8ebe4c7e465b2b3a481ee245623ee
i62cogag <i62cogag@uco.es>
2019-10-01 17:56:48 +0200

    Added Cmake

---
+++ p1/CMakeFiles/practica1.exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practica1.exe.dir/main.cpp.o"
+  "CMakeFiles/practica1.exe.dir/quicksort.cpp.o"
+  "CMakeFiles/practica1.exe.dir/matrix.cpp.o"
+  "practica1.exe.pdb"
+  "practica1.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practica1.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
