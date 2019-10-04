65913962a52eac3e9ecbcc82346f19c6336d32a9
i62cogag <i62cogag@uco.es>
2019-10-04 19:57:00 +0200

    Funciona correcto, falta calcular el tiempo estimado para un N grande

--- p1/matrix.hpp
+++ p1/matrix.hpp
@@ -17,5 +17,9 @@ using namespace std;
 void rellenarMatriz(vector<vector<double>> &v);
 void redimensionaMatriz(vector<vector<double>> &v,int tam);
 void productoMatrices(vector<vector<double>> &v,vector<vector<double>> &matriz_resultado);
+void ajustePolinomico(const vector<double> &n, const vector<double> &times, vector <double> &a);
+double sumatorio(vector<double> vec1, vector<double> vec2, double exp1, double exp2);
+void calcularTiemposEstimadosPolinomio(const vector<double> &n, const vector<double> &times, const vector<double> &a, vector<double> &estimated_times);
+double calcularTiempoEstimadoPolinomico(const double &n,const vector<double> &a);
 
 #endif
--- p1/quicksort.cpp
+++ p1/quicksort.cpp
@@ -8,7 +8,6 @@ void rellenarVector(vector <int> &v){
     }
 }
 
-
 bool estaOrdenado(const vector<int> &v){
     bool is_ordered = true;
     for(int i = 0; i < v.size()-1; i++){
@@ -58,27 +57,26 @@ int divide(vector<int> &v, int start, int end){
     return right;
 }
 
-bool escribeFichero(vector<double> &n,vector<double> &times,vector<double> &estimates_times){
+bool escribeFichero(vector<double> &n,vector<double> &times,vector<double> &estimated_times,string file_name){
     bool escrito = false;
     #ifndef NDEBUG
         assert( !n.empty() );
         assert( !times.empty() );
-        assert( !estimates_times.empty());
+        assert( !estimated_times.empty());
     #endif
 
-    ofstream file("Datos.txt");
-
+    ofstream file(file_name);
     for(int i = 0; i < n.size(); i++){
-        file << n[i] << " " << times[i] << " " << estimates_times[i] << "\n";
+        file << n[i] << " " << times[i] << " " << estimated_times[i] << "\n";
     }
 
     file.close();
-
     escrito = true;
 
     #ifndef NDEBUG
         assert( escrito == true );
     #endif
+
     return escrito;
 }
 
@@ -89,7 +87,7 @@ void ajusteNlogN(const vector<double> &n,const vector<double> &times,double &a0,
 
     std::vector<double> z;
     z.resize(n.size());
-    //calculo del cambio de variable
+    //cálculo del cambio de variable
     for(int i = 0; i < z.size(); i++){
         z[i] = n[i] * log10(n[i]);
     }
@@ -105,32 +103,13 @@ void ajusteNlogN(const vector<double> &n,const vector<double> &times,double &a0,
         }
         B[i][0] = sumatorio(z,times,i,1);
     }
-
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 2; j++){
-            cout<<"A["<<i<<"]["<<j<<"] = "<<A[i][j]<<"\n";
-            cout<<"B["<<i<<"]["<<j<<"] = "<<B[i][j]<<"\n";
-        }
-    }
     //resolvemos el sistema de ecuaciones
     resolverSistemaEcuaciones(A,B,2,X);
     a0 = X[0][0];
     a1 = X[1][0];
 }
 
-double sumatorio(vector<double> vec1, vector<double> vec2, double exp1, double exp2){
-    #ifndef NDEBUG
-        assert( vec1.size() == vec2.size());
-    #endif
-    double sum = 0;
-    for(int i = 0; i < vec1.size(); i++){
-        sum += pow(vec1[i],exp1) * pow(vec2[i],exp2);
-    }
-    return sum;
-}
-
 void calcularTiemposEstimadosNlogN(const vector<double> &n,const vector<double> &times, const double &a0, const double &a1, vector<double> &estimated_times){
-
     for(int i = 0; i < times.size(); i++){
         estimated_times.push_back(a0 + a1 * n[i]*log10(n[i]));
     }
