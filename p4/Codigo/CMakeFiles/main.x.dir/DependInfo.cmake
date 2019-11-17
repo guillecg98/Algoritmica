30d4b060541b58f43bc02bd165f5504ab61f0864
Guille Cosano <i62cogag@uco.es>
2019-11-17 18:31:35 +0100

    Cmake stuff

---
+++ p4/Codigo/CMakeFiles/main.x.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/algorithm.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/algorithm.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/digitalcurve.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/digitalcurve.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/funcionesAuxiliares.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/funcionesAuxiliares.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/greedymethod.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/greedymethod.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/line.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/line.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/main.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/main.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/point.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/point.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/suppressioncollinearpointsmethod.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p4/Codigo/CMakeFiles/main.x.dir/suppressioncollinearpointsmethod.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
