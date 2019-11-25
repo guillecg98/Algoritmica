c467babc08a43b41f0ef6f568c49f8a7852c9e39
Guille Cosano <i62cogag@uco.es>
2019-11-25 17:49:15 +0100

    cmake stuff

---
+++ p5/Codigo/CMakeFiles/main.x.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/algorithm.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/algorithm.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/digitalcurve.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/digitalcurve.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/dynamicprogrammingmethod.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/dynamicprogrammingmethod.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/funciones.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/funciones.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/line.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/line.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/main.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/main.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/point.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/point.cpp.o"
+  "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/suppressioncollinearpointsmethod.cpp" "/home/gecoga98/Desktop/4º/Algoritmica/p5/Codigo/CMakeFiles/main.x.dir/suppressioncollinearpointsmethod.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ p5/Codigo/CMakeFiles/main.x.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.x.dir/main.cpp.o"
+  "CMakeFiles/main.x.dir/funciones.cpp.o"
+  "CMakeFiles/main.x.dir/algorithm.cpp.o"
+  "CMakeFiles/main.x.dir/digitalcurve.cpp.o"
+  "CMakeFiles/main.x.dir/suppressioncollinearpointsmethod.cpp.o"
+  "CMakeFiles/main.x.dir/point.cpp.o"
+  "CMakeFiles/main.x.dir/line.cpp.o"
+  "CMakeFiles/main.x.dir/dynamicprogrammingmethod.cpp.o"
+  "main.x.pdb"
+  "main.x"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
