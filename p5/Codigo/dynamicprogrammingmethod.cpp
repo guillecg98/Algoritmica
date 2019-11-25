abd94d37601e431ed4b9023c5e2d915baae07bf1
Guille Cosano <i62cogag@uco.es>
2019-11-25 20:00:08 +0100

    Practica terminada, hay que corregir el error ya que sale muy mal

--- p5/Codigo/dynamicprogrammingmethod.cpp
+++ p5/Codigo/dynamicprogrammingmethod.cpp
@@ -17,12 +17,13 @@ void DynamicProgrammingMethod::apply(){
     calculateSummations();
 
     double min;
+    int posAnterior;
     std::vector<int> interestPointsPosition;
     int puntosCurva = getOriginalCurve().getNumberPointsDigitalCurve();
     int puntosAproximacion = getNumberPointsPolygonalApproximation();
 
-    Matrix E(puntosCurva,puntosAproximacion);
-    Matrix Father(puntosCurva,puntosAproximacion);
+    Matrix E(puntosCurva+1,puntosAproximacion+1);
+    Matrix Father(puntosCurva+1,puntosAproximacion+1);
 
     //Rellenamos la primera columna de la matriz de errores con los casos particulares
     for(int m = 1; m < 2; m++){
@@ -31,22 +32,34 @@ void DynamicProgrammingMethod::apply(){
             E.setElement(n,m,9999);
         }
     }
-    E.printMatrix();
+
     /*              ALGORITMO                          */
-    /*for(int m = 2; m < puntosAproximacion; m++){
-        for(int n = 2; n < puntosCurva; n++){
+    for(int m = 2; m <= puntosAproximacion; m++){
+        for(int n = 2; n <= puntosCurva; n++){
             min = E.getElement(2,1);
             for(int j = m-1; j <= n-1; j++){
-                if(){
-                min = E[m-1][m-1] + calculateISEValue(j-1,n-1);
+                if( (E.getElement(j,m-1) + calculateISEValue(j-1,n-1)) < min){
+                min = E.getElement(j,m-1) + calculateISEValue(j-1,n-1);
+                posAnterior = j;
                 }
             }
-            E[n][m] = min;//error minimo
-            Father[n][m] = 1;//
+            E.setElement(n,m,min);//error minimo
+            Father.setElement(n,m,posAnterior);//
         }
-    }*/
+    }
 
+    int i = Father.getRows()-1;
+    int j = Father.getColumns()-1;
+    while(Father.getElement(i,j) != 0){
+        if( Father.getElement(i,j) == Father.getElement(i-1,j) ){
+            i--;
+        }else{
+            interestPointsPosition.push_back(Father.getElement(i-1,j));
+            j --;
+        }
+    }
 
+    std::cout<<"Tamaño dominantes = "<<interestPointsPosition.size()<<"\n";
 
     setDominantPointsPosition(interestPointsPosition);//finalmente seteamos el vector resultado al algoritmo
     //se llama a la funcion encargada de calcular la aproximacion poligonal
