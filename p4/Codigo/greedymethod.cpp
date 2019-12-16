483d2ca8193a6b9f2d114e4d508be99f8ae11ccc
Guille Cosano <i62cogag@uco.es>
2019-12-16 21:24:47 +0100

    Finish

--- p4/Codigo/greedymethod.cpp
+++ p4/Codigo/greedymethod.cpp
@@ -35,37 +35,34 @@ void GreedyMethod::apply()
 {
     calculateSummations();
     collinearPointsElimination();
-    //si la curva es cerrada, quitamos un punto de la aproximacion poligonal
+
     if(getOriginalCurve().isClosed()){
-        setNumberPointsPolygonalApproximation(getNumberPointsPolygonalApproximation() -1);
+        setNumberPointsPolygonalApproximation(getNumberPointsPolygonalApproximation()-1);
     }
-    //calculamos el tamaño del paso. tamaño del vector de dominantes entre (numero de puntos de la aproximacion - 1)
-    int paso = (getDominantPointsPosition().size() / (getNumberPointsPolygonalApproximation() -1));
-    //Rellenamos el nuevo vector de dominantes cumpliendo con los pasos
+    int paso = getDominantPointsPosition().size() / (getNumberPointsPolygonalApproximation() -1);
+
     std::vector<int> nuevosDominantes;
     for(int i = 0; i < getDominantPointsPosition().size(); i+=paso){
         nuevosDominantes.push_back(getDominantPointsPosition()[i]);
     }
-    //si el ultimo de dominantes y el ultimo de nuevos dominantes no son el mismo, forzamos a que lo sea
+
     if(nuevosDominantes.back() != getDominantPointsPosition().back()){
         nuevosDominantes.push_back(getDominantPointsPosition().back());
     }
-    //Algoritmo:
-    int nuevoPunto = 0;
-    float minError,error;
+
+    int nuevo = 0;
+    float minError, error;
     for(int i = 1; i < getNumberPointsPolygonalApproximation(); i++){
         minError = 9999;
-        for(int j = nuevosDominantes[i-1] + 1; j <= nuevosDominantes[i+1]; j++){
+        for(int j = nuevosDominantes[i-1]+1; j <= nuevosDominantes[i+1]; j++){
             error = calculateISEValue(nuevosDominantes[i-1],j) + calculateISEValue(j,nuevosDominantes[i+1]);
             if(error < minError){
                 minError = error;
-                nuevoPunto = j;
+                nuevo = j;
             }
         }
-        nuevosDominantes[i] = nuevoPunto;
+        nuevosDominantes[i] = nuevo;
     }
-
-    //insertamos el nuevo vector de dominantes en  el vector de dominantPoints
     setDominantPointsPosition(nuevosDominantes);
     calculatePolygonalApproximation();
 }
